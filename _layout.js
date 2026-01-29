909ca100b952fc5a7660484571606f8d1bdeb3f6
samuellvo <samuel.vo@my.gavilan.edu>
2026-01-28 19:04:25 -0800

    Added init skeleton pages -- landing and auth.

---
+++ _layout.js
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import { PaperProvider } from "react-native-paper";
+
+
+// _layout file is found by expo router automatically to create a stack for pages traversed.
+// it keeps track of visited pages and pops or appends during travel forwards and back.
+
+export default function RootLayout() {
+    return(
+        <PaperProvider>
+            <Stack />
+        </PaperProvider>
+    );
+}
---
+++ app/index.js
@@ -0,0 +1,34 @@
+import { StyleSheet, View } from 'react-native';
+import {Button, Text} from "react-native-paper";
+import {useRouter} from "expo-router";
+
+export default function Index() {
+  const router = useRouter();
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={{marginBottom: 20, fontSize: 30, fontWeight: 600}} variant="headlineMedium"> Imposter Game </Text>
+      <Button mode="contained" onPress={() => router.push("/login")}>
+        Login
+      </Button>
+
+      <Button mode="contained" onPress={() => router.push("/home")}>
+        Guest
+      </Button>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    gap: 12,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {borderRadius: 5}
+});
